9efa3fdb3c575a2baefcc65bf4ca6ca470094fd7
Vicente <vicente@carmendata.co.uk>
2016-10-03 14:51:31 +0100

    Removed useless file

--- admin/wp-comcar-plugins-admin-html.php
+++ admin/wp-comcar-plugins-admin-html.php
@@ -23,6 +23,10 @@ add_action( 'admin_menu', 'add_settings_page' );
 
 
 
+
+function plugin_options_page() {
+        include 'wp-comcar-plugins-admin-options.php';
+}
 /*---------------------------------------------------
 register settings
 ----------------------------------------------------*/
@@ -30,9 +34,13 @@ function plugin_settings_init(){
     register_setting( 'plugin_settings', 'plugin_settings' );
     wp_register_script( 'panel_script' , plugins_url( '/js/wp-comcar-plugins-admin.js', __FILE__ ) );
     wp_register_style( 'panel_style' , plugins_url( '/css/wp-comcar-plugins-admin.css', __FILE__ ));
-    
+   
     wp_enqueue_script('panel_script');
     wp_enqueue_style('panel_style');      
+  
+
+// Nav options
+    // add_options_page('Comcar Plugin Menu', 'Comcar Plugin Menu', 'manage_options', 'WPComcar_plugin', 'plugin_options_page');
 }
 
 
@@ -54,26 +62,74 @@ function plugin_settings_page() {
     <div class="wrap">
         <h2 class="nav-tab-wrapper">';
 
-    foreach($structure as $arrKey => $arrTitle){ 
+    $str_settings = '<div id="WPComcar_sections">';
+    
+    foreach($structure as $arrKey => $arrTitle) { 
         $str_function_name = 'WPComcar_plugin_options_'.$arrKey; 
         if (strcmp($arrKey,"general")==0){
             echo '<a class="nav-tab WPComcar_subTab nav-tab-active '.$arrKey.'" data-targettab="' .  $str_function_name . '">'.$arrTitle.'</a>';
         }else{
             echo '<a class="nav-tab WPComcar_subTab '.$arrKey.'" data-targettab="' .  $str_function_name . '">'.$arrTitle.'</a>';
-        }               
+        } 
+        // $str_settings .= call_user_func( 'WPComcar_plugin_options_' . $arrKey );              
     }
+
+    $str_settings = '</div>';
+
 
     echo '
         </h2>   
     </div>
     ';
+    echo $str_settings;
+    
     submit_button("Save Changes"); 
 }
 
 
+
 function WPComcar_plugin_options_general(){
-    echo 'main';
+ add_settings_field( 'myprefix_setting-id',
+    'This is the setting title',
+    'myprefix_setting_callback_function',
+    'general',
+    'myprefix_settings-section-name',
+    array( 'label_for' => 'myprefix_setting-id' ) );
+
 }
+
+
+
+
+function WPComcar_plugin_options_tax_calculator(){
+    echo 'tax calculator';
+}
+
+
+
+
+
+function WPComcar_plugin_options_comparator(){
+    echo 'comparator';
+}
+
+
+
+
+function WPComcar_plugin_options_electric_comparator(){
+    echo 'electric comparator';
+}
+
+
+
+
+function WPComcar_plugin_options_footprint(){
+    echo 'footprint';
+}
+
+
+
+
 
 
 
