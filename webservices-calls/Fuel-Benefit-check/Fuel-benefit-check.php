105fbae53e5ba86fab094f1275e28a184f0701a0
Vicente <vicente@carmendata.co.uk>
2016-10-26 09:12:59 +0100

    Fuel benefit check > More tidy up

--- webservices-calls/Fuel-Benefit-check/Fuel-benefit-check.php
+++ webservices-calls/Fuel-Benefit-check/Fuel-benefit-check.php
@@ -1,36 +1,27 @@
 <?php 
-
 	global $post;
-	$thePostId=$post->ID;
-	$arrOptions = get_option('WP_plugin_options_fuel_benefit_check'); 
-	$idThePageWhereShouldLoadThePlugin = $arrOptions["fuel_benefit_check_page"];
+	$thePostId = $post->ID;
+	$WPComcar_arrOptions = get_option( 'WP_plugin_options_fuel_benefit_check' ); 
+	$idThePageWhereShouldLoadThePlugin = $WPComcar_arrOptions["fuel_benefit_check_page"];
 
 	if (  $idThePageWhereShouldLoadThePlugin == $thePostId  ) {
-		include_once (WPComcar_WEBSERVICESCALLSPATH.'Carmen-Data-Web-Services-Common-Files/requiredForCarTools.php');
-    	$WPComcar_objConfig = $_POST;
+		include_once ( WPComcar_WEBSERVICESCALLSPATH.'Carmen-Data-Web-Services-Common-Files/requiredForCarTools.php' );
 
 	    // Convert structure into JSON
-	    $WPComcar_jsnConfig = json_encode( $WPComcar_objConfig );
+	    $WPComcar_jsnConfig = json_encode(  $_POST );
 
     	try {
 	        // connect to the webservice
 	        $WPComcar_ws = new SoapClient( $WPComcar_services['fuelBenefit'], array( 'cache_wsdl' => 0 ) );
-
-	        // call the required functions and store the returned data
 	        $WPComcar_resultsJS = fixForSsl( $WPComcar_ws -> GetJS( $WPComcar_pubhash, $WPComcar_clk, 1 ) );
-	        $WPComcar_arrOptions = get_option("WP_plugin_options_fuel_benefit_check");
-	        $WPComcar_actionName =  get_permalink($WPComcar_arrOptions['fuel_benefit_check_subpage_model']); 
-
-	        //CHANGE THE FORM SUBMISSION TO THE NEXT PAGE in Wordpress
-	        $WPComcar_arrOptions = get_option( "WP_plugin_options_fuel_benefit_check" );
-	        $WPComcar_resultsHTML = fixForSsl( $WPComcar_ws -> GetHTML( $WPComcar_pubhash, $WPComcar_clk,2,$WPComcar_actionName,$WPComcar_jsnConfig ));
+	        $WPComcar_resultsHTML = fixForSsl( $WPComcar_ws -> GetHTML( $WPComcar_pubhash, $WPComcar_clk,$WPComcar_jsnConfig ));
 
     	} catch (Exception $WPComcar_e) {
 	        // Error handling code if soap request fails 
 	        $WPComcar_msg = $WPComcar_msg.'The webservice failed to load the selector<br />';
     	}
     
-    include_once ( WPComcar_WEBSERVICESCALLSPATH . 'Carmen-Data-Web-Services-Template/template.php');
+    	include_once ( WPComcar_WEBSERVICESCALLSPATH . 'Carmen-Data-Web-Services-Template/template.php');
 	} 
 
 ?>
