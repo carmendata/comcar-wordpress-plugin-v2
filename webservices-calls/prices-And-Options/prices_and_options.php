e9baa8e11c9eaae8d120bef66816d149fb606cdb
Vicente <vicente@carmendata.co.uk>
2016-10-31 14:08:00 +0000

    Web services > prices and options: make works two first steps (select and model)

--- webservices-calls/prices-And-Options/prices_and_options.php
+++ webservices-calls/prices-And-Options/prices_and_options.php
@@ -10,27 +10,28 @@
     if (  $idThePageWhereShouldLoadThePlugin == $thePostId  ) {
         include_once ( WPComcar_WEBSERVICESCALLSPATH.'Carmen-Data-Web-Services-Common-Files/requiredForCarTools.php' );
 
-        // Convert structure into JSON
-        $WPComcar_jsnConfig = json_encode(  $_POST );
 
+
+
+
+        // Convert structure into JSON
+        $WPComcar_jsonPost = json_encode(  $_POST );
+        $WPComcar_jsonGet = json_encode(  $_GET );
         try {
             // connect to the webservice
-            $WPComcar_ws = new SoapClient( $WPComcar_services['carDetails'], array( 'cache_wsdl' => 0 ) );
+            $WPComcar_ws = new SoapClient( $WPComcar_services['pricesAndOptions'], array( 'cache_wsdl' => 0 ) );
             $stage = "select";
-            
+           
+            if (array_key_exists( 'fuelType', $_GET )) {
+                $stage = "model";
+            }    
+         
+            if (array_key_exists( 'ID', $_GET )) {
+                $stage = "options";            
+            }   
 
-
-
-            if ( array_key_exists('fmake',$_POST) ) {
-                $stage = "details";
-            } 
-            
-
-
-
-
-            $WPComcar_resultsHTML = fixForSsl( $WPComcar_ws -> GetHTML( $WPComcar_pubhash, $WPComcar_clk,$WPComcar_jsnConfig ));
-            $WPComcar_resultsJS = fixForSsl( $WPComcar_ws -> GetJS( $WPComcar_pubhash, $WPComcar_clk,   $stage ) );
+            $WPComcar_resultsHTML = fixForSsl( $WPComcar_ws -> GetHTML( $WPComcar_pubhash, $WPComcar_clk,$WPComcar_jsonPost, $WPComcar_jsonGet  ));
+            $WPComcar_resultsJS = fixForSsl( $WPComcar_ws -> GetJS( $WPComcar_pubhash, $WPComcar_clk,   $stage,  $WPComcar_jsonGet ) );
             $WPComcar_resultsCSS = fixForSsl( $WPComcar_ws -> GetCSS( $WPComcar_pubhash, $WPComcar_clk,   $stage ) );
                 
         } catch ( Exception $WPComcar_e ) {
