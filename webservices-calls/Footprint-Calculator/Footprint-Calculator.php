444d6560146624d2d522350649c375a850f9156d
Vicente <vicente@carmendata.co.uk>
2017-01-13 15:58:08 +0000

    Fix bug with HTTPS in footprint

--- webservices-calls/Footprint-Calculator/Footprint-Calculator.php
+++ webservices-calls/Footprint-Calculator/Footprint-Calculator.php
@@ -21,8 +21,8 @@
 		// connect to the webservice
 		$WPComcar_ws = new SoapClient($WPComcar_services['footprint'], array('cache_wsdl' => 0));
 		// call the required functions and store the returned data
-		$WPComcar_resultsJS = $WPComcar_ws->GetJS($WPComcar_pubhash, $WPComcar_clk);
-		$WPComcar_resultsHTML = $WPComcar_ws->GetHTML($WPComcar_pubhash, $WPComcar_clk, 'Footprint-Calculator.php', $WPComcar_jsnDataAndConfig);
+		$WPComcar_resultsJS = fixForSsl($WPComcar_ws->GetJS($WPComcar_pubhash, $WPComcar_clk));
+		$WPComcar_resultsHTML = fixForSsl($WPComcar_ws->GetHTML($WPComcar_pubhash, $WPComcar_clk, 'Footprint-Calculator.php', $WPComcar_jsnDataAndConfig));
 		
 	} catch (Exception $e) {
 		// Error handling code if soap request fails 
