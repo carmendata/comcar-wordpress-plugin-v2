8a082b5935e5b1b1acf78b8fc0e0f2d84accd17e
Peter Williamson <pete@willshawmedia.com>
2022-04-25 05:42:54 +0100

    bump patch number, remove comments

--- wp-comcar-plugins.php
+++ wp-comcar-plugins.php
@@ -13,7 +13,7 @@
 // ini_set( 'error_reporting', E_ALL );
 // ini_set( 'display_errors', true );
 
-define("WP_COMCAR_PLUGINS_PLUGINVERSION","2.0.0");
+define("WP_COMCAR_PLUGINS_PLUGINVERSION","2.0.2");
 define("WP_COMCAR_PLUGINS_WEBSERVICECONTENT",dirname(__FILE__)."/webservices-calls/");
 
 # set default webservice URL if not defined in environment
@@ -37,11 +37,4 @@ require_once( __DIR__."/admin/wp-comcar-plugins-admin.php" );
 require_once( __DIR__."/wp-comcar-plugins-content.php" );
 
 add_filter( "the_content",  "getToolContent" );
-
-// add_action( "wp", "plugin_redirection" );
-// add_action( "wp_head", "activate_page_plugins");
-
-// add_action( 'wp_head', 'wp_buttons' );
-// add_action( 'wp_head', 'mpg_scripts' );
-// add_action( 'wp_head', 'vue_test_scripts_and_css' );
 ?>
